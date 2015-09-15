e9928c557bc40f098c5363c53d10fe77f02977b1
James Milne <jmilne@graphic-designer.com>
2015-09-15 13:02:36 +1000

    Added version example to demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,12 @@
 import hug
 from bottle import template
 
-@hug.get('/happy_birthday', output=hug.output_format.html)
+@hug.get('/happy_birthday', output=hug.output_format.html, versions=1)
 def happy_birthday(name, age:hug.types.number=1):
     """Says happy birthday to a user"""
     return template('<b>Happy {{age}} Birthday, {{name}}</b>', age=age, name=name).format('HTML')
+
+@hug.get('/happy_birthday', output=hug.output_format.html, versions=range(2, 5))
+def happy_birthday(name, age:hug.types.number=1):
+    """Says happy birthday to a user"""
+    return template('<b>Happy Birthday, {{name}}, on your {{age}}nd birthday!</b>', age=age, name=name).format('HTML')
